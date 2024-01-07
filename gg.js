3ba67f5edf49aa908f5c34dd6df688318a3e7cd5
Temuulen <temuulenb80@gmail.com>
2024-01-08 06:51:19 +0800

    home

--- gg.js
+++ gg.js
@@ -1,95 +1,385 @@
-let taskArr = [
-  {
-    title: "Task1",
-    description: "Task1",
-    status: "To Do",
-    priority: "Low",
-    id: 1,
-    button: "Add Task",
-  },
-  {
-    title: "Task2",
-    description: "Task2",
-    status: "In-progress",
-    priority: "Low",
-    id: 2,
-    button: "Add Task",
-  },
-  {
-    title: "Task3",
-    description: "Task3",
-    status: "Stuck",
-    priority: "Low",
-    id: 3,
-    button: "Add Task",
-  },
-  {
-    title: "Task4",
-    description: "Task4",
-    status: "Done",
-    priority: "Low",
-    id: 4,
-    button: "Add Task",
-  },
-];
-// root ee duudan
-const root = document.getElementById("root");
-boardArr = ["To-Do", "In-progress", "Stuck", "Done"];
+//asuult nohtsoloos hamaarch ali function ajillah we gedgiig shiidex. (edit hiih uyd add task darhad)
+//asuult yaaj arrow function-iig duudal ajiluulah we
 
-// boards hiij bn
+let root = document.getElementById("root");
+let boardsArr = ["To do", "In progress", "Stuck", "Done"];
+let taskArr = [
+  // {
+  //   Description: "sdfg",
+  //   PriorNum: 3,
+  //   Priority: "High",
+  //   Status: "To do",
+  //   Title: "1234",
+  //   id: 1,
+  // },
+];
+let idNum = 1;
+//let todoArr = [];
+// let inprogArr = new Array();
+
 let boards = document.createElement("div");
 boards.setAttribute("class", "boards");
 root.appendChild(boards);
 
-function renderTasks(list) {
-  boardArr.map((event) => {
-    // board iig hiij bn
-    let board = document.createElement("div");
-    board.setAttribute("class", "board");
-    board.setAttribute("id", `${event}`);
-    boards.appendChild(board);
+//board bolon dotorhi zuiluudiig hiij baigaa heseg
+boardsArr.map((event) => {
+  //board hiij baigaa
+  let board = document.createElement("div");
+  board.setAttribute("class", "board");
+  board.setAttribute("id", event);
+  boards.appendChild(board);
 
-    // board header
-    let header = document.createElement("h2");
-    header.setAttribute("class", "boardHeader");
-    header.setAttribute("id", `${event}`);
-    board.appendChild(header);
+  //boardiin title hiij baigaa
+  let title = document.createElement("h2");
+  title.setAttribute("class", "boardHeader");
+  title.innerText = `${event}`;
+  title.setAttribute("id", `${event}`);
+  board.appendChild(title);
 
-    // board card
-    let card = document.createElement("div");
-    card.setAttribute("class", "card");
-    card.setAttribute("id", `${event}`);
-    board.appendChild(card);
+  //count number
+  let countNum = document.createElement("p");
+  countNum.setAttribute("id", `count${event}`);
+  board.appendChild(countNum);
+  boards.appendChild(board);
 
-    // board button
-    let button1 = document.createElement("button");
-    button1.setAttribute("class", "button");
-    board.appendChild(button1);
+  //todo card hiij baigaa
+  let cards = document.createElement("div");
+  cards.setAttribute("class", "cards");
+  cards.setAttribute("id", `cards${event}`);
+  board.appendChild(cards);
+  //add card button hiij baigaa heseg
+  let button = document.createElement("button");
+  button.setAttribute("class", "button");
+  button.addEventListener("click", showPopup);
+  button.innerText = "+Add card";
+  board.appendChild(button);
+});
 
-    const taskDone = list.filter((todo) => {
-      return todo.status == `${event}`;
-    });
-    const doneStatusDiv = document.createElement("div");
-    doneStatusDiv.setAttribute("class", "status_column");
+//Add task pop up screen-iig hiij baigaa heseg
+let taskProperties = ["Title", "Description", "Status", "Priority"];
+let popup = document.createElement("div");
+popup.setAttribute("class", "popup");
+root.appendChild(popup);
 
-    const statusHeadD = document.createElement("div");
-    statusHeadD.setAttribute("class", "status_head");
-    statusHeadD.innerHTML = `${event}`;
-    doneStatusDiv.appendChild(statusHeadD);
+let popupHeader = document.createElement("h2");
+popupHeader.setAttribute("class", "popupHeader");
+popupHeader.setAttribute("id", "popupHeader");
+popupHeader.innerText = "Add task";
+popup.appendChild(popupHeader);
 
-    const heatCountD = document.createElement("span");
-    heatCountD.innerHTML = taskTodo.length;
-    statusHeadD.appendChild(heatCountD);
-  });
+//popup-iin content-uudiig ni hiij baigaa
+taskProperties.map((event) => {
+  if (event == "Title" || event == "Description") {
+    //div-uudiig hiilee
+    console.log(event);
+    let property = document.createElement("div");
+    property.setAttribute("class", "property");
+    property.setAttribute("id", `${event}`);
+    popup.appendChild(property);
 
-  taskDone.map((task) => {
-    const newTask = createTask(task);
+    //property dotorhi content-uudiig hiij baigaa
+    let label = document.createElement("label");
+    label.setAttribute("class", "label");
+    label.innerText = `${event}`;
+    property.appendChild(label);
 
-    doneStatusDiv.appendChild(newTask);
-    container.appendChild(doneStatusDiv);
-  });
+    //property dotorhi content-uudiig hiij baigaa
+    let input = document.createElement("input");
+    input.setAttribute("class", "input");
+    input.setAttribute("id", `input${event}`);
+    property.appendChild(input);
+  } else if (event == "Status") {
+    //div-uudiig hiilee
+    let property = document.createElement("div");
+    property.setAttribute("class", "property");
+    property.setAttribute("id", `${event}`);
+    popup.appendChild(property);
 
-  const addCardButton3 = addCard();
-  doneStatusDiv.appendChild(addCardButton3);
+    //property dotorhi content-uudiig hiij baigaa
+    let label = document.createElement("label");
+    label.setAttribute("class", "label");
+    label.innerText = `${event}`;
+    property.appendChild(label);
+
+    let input = document.createElement("select");
+    input.setAttribute("class", "input");
+    input.setAttribute("id", `input${event}`);
+
+    let option1 = document.createElement("option");
+    option1.value = `To do`;
+    option1.innerText = `To do`;
+    input.appendChild(option1);
+    let option2 = document.createElement("option");
+    option2.value = `In progress`;
+    option2.innerText = `In progress`;
+    input.appendChild(option2);
+    let option3 = document.createElement("option");
+    option3.value = `Stuck`;
+    option3.innerText = `Stuck`;
+    input.appendChild(option3);
+    let option4 = document.createElement("option");
+    option4.value = `Done`;
+    option4.innerText = `Done`;
+    input.appendChild(option4);
+    property.appendChild(input);
+  } else if (event == "Priority") {
+    //div-uudiig hiilee
+    let property = document.createElement("div");
+    property.setAttribute("class", "property");
+    property.setAttribute("id", `${event}`);
+    popup.appendChild(property);
+
+    //property dotorhi content-uudiig hiij baigaa
+    let label = document.createElement("label");
+    label.setAttribute("class", "label");
+    label.innerText = `${event}`;
+    property.appendChild(label);
+
+    let input = document.createElement("select");
+    input.setAttribute("class", "input");
+    input.setAttribute("id", `input${event}`);
+
+    let option1 = document.createElement("option");
+    option1.value = "High";
+    option1.innerText = `High`;
+    input.appendChild(option1);
+    let option2 = document.createElement("option");
+    option2.value = "Medium";
+    option2.innerText = `Medium`;
+    input.appendChild(option2);
+    let option3 = document.createElement("option");
+    option3.value = "Low";
+    option3.innerText = `Low`;
+    input.appendChild(option3);
+    property.appendChild(input);
+  }
+});
+
+//popup-iin button hiij baigaa heseg
+let popupButton = document.createElement("button");
+popupButton.setAttribute("class", "button");
+popupButton.setAttribute("id", "popupButton");
+popupButton.innerText = "Add task";
+// popupButton.setAttribute("click", addTask);
+popupButton.addEventListener("click", addTaskButtF);
+popup.appendChild(popupButton);
+popup.style.display = "none";
+
+//add card darhad popup garch ireh
+function showPopup() {
+  popup.style.display = "block";
 }
-renderTasks(taskArr);
+
+//add task fn (pop-up)
+//  input value-g barij awna
+//  barij awsan value-uudiig object bolgono
+//  tom array-ruu object-iig push hiine
+//  popup window disappear
+//  render fn duudaj ajiluulna
+function addTaskButtF() {
+  let tempObj = {
+    id: idNum,
+    Title: document.getElementsByClassName("input")[0].value,
+    Description: document.getElementsByClassName("input")[1].value,
+    Status: document.getElementsByClassName("input")[2].value,
+    Priority: document.getElementsByClassName("input")[3].value,
+  };
+
+  //Sort hiihiin tuld Priority-d hargalzah utga onoogood tempObjruu hiij baigaa
+  if (document.getElementsByClassName("input")[3].value == "High") {
+    tempObj.PriorNum = 3;
+  } else if (document.getElementsByClassName("input")[3].value == "Medium") {
+    tempObj.PriorNum = 2;
+  } else if (document.getElementsByClassName("input")[3].value == "Low") {
+    tempObj.PriorNum = 1;
+  }
+
+  // let tempPri = document.getElementsByClassName("input")[3].value;
+  taskArr.push(tempObj);
+  console.log(taskArr);
+
+  //task-iin input-iig hoosloj baigaaa
+  document.getElementsByClassName("input")[0].value = "";
+  document.getElementsByClassName("input")[1].value = "";
+  document.getElementsByClassName("input")[2].value = "";
+  document.getElementsByClassName("input")[3].value = "";
+  popup.style.display = "none";
+  idNum++;
+  arrangeAndRender();
+}
+
+//arrange and render fn:
+//  cards dotor card bolhoj hiine
+function arrangeAndRender() {
+  //shine card nemhiin omno huuchin card-uudiig ustgaj baigaa
+  for (let i = 0; i < 4; i++) {
+    document.getElementById(`cards${boardsArr[i]}`).innerHTML = "";
+  }
+  //  tom array-g filterdeed 4 jijig array bolgono
+  let todoArr = [];
+  for (let i = 0; i < 4; i++) {
+    todoArr[i] = taskArr.filter((event) => {
+      return event.Status == boardsArr[i];
+    });
+
+    todoArr[i].sort((a, b) => {
+      return b.PriorNum - a.PriorNum;
+    });
+  }
+
+  //count number ajiluulj hiij baigaa heseg
+  todoArr.map((event, index) => {
+    let countNum = document.getElementById(`count${boardsArr[index]}`);
+    if (event.length == 0) {
+      countNum.innerText = `0`;
+    } else {
+      countNum.innerText = `${event.length}`;
+    }
+  });
+
+  //  card-iin div uusgeed
+  //  dotor ni content-uudaa hiij ogj baigaa heseg
+  //  cards-ruu hiij baigaa
+  for (let i = 0; i < 4; i++) {
+    todoArr[i].map((event) => {
+      let card = document.createElement("div");
+      card.setAttribute("class", "card");
+
+      let tick = document.createElement("button");
+      tick.innerText = `Done`;
+      card.appendChild(tick);
+
+      let head = document.createElement("h3");
+      head.innerText = `${event.Title}`;
+      card.appendChild(head);
+
+      let desc = document.createElement("p");
+      desc.innerText = `${event.Description}`;
+      card.appendChild(desc);
+
+      let prio = document.createElement("button");
+      prio.innerText = `${event.Priority}`;
+      card.appendChild(prio);
+
+      let edit = document.createElement("button");
+      edit.setAttribute("id", `${event.id}`);
+      edit.addEventListener("click", (event) => {
+        let id = event.target.id;
+        editF(id);
+      });
+      edit.innerText = `Edit`;
+      card.appendChild(edit);
+
+      let dele = document.createElement("button");
+      dele.innerText = `Delete`;
+      dele.setAttribute("id", `${event.id}`);
+      dele.addEventListener("click", (event) => {
+        let id = event.target.id;
+        deleteF(id);
+      });
+      card.appendChild(dele);
+
+      //zow cards-ruu zow array-iig ni oruulj ogj baigaa heseg
+      document.getElementById(`cards${boardsArr[i]}`).appendChild(card);
+    });
+  }
+  document.getElementById("popupButton").innerText = "Add task";
+  document.getElementsByClassName("popupHeader").innerText = "Add task";
+}
+function editF(id) {
+  let tempObj = taskArr.filter((event) => {
+    return event.id == id;
+  });
+
+  for (let i = 0; i < 4; i++) {
+    if (i == 0) {
+      document.getElementById(`input${taskProperties[i]}`).value =
+        tempObj[0].Title;
+    } else if (i == 1) {
+      document.getElementById(`input${taskProperties[i]}`).value =
+        tempObj[0].Description;
+    } else if (i == 2) {
+      document.getElementById(`input${taskProperties[i]}`).value =
+        tempObj[0].Status;
+    } else if (i == 3) {
+      document.getElementById(`input${taskProperties[i]}`).value =
+        tempObj[0].Priority;
+    }
+  }
+
+  taskArr = taskArr.filter((event) => {
+    return event.id != id;
+  });
+  document.getElementById("popupButton").innerText = "Confirm Edit";
+  document.getElementById("popupHeader").innerText = "Edit task";
+
+  showPopup();
+}
+
+function deleteF(id) {
+  taskArr = taskArr.filter((event) => {
+    return event.id != id;
+  });
+  arrangeAndRender();
+}
+
+//  Done----------------------------------------------------------------
+//add card darhad popup garch ireh
+//add task fn (pop-up)
+//  input value-g barij awna
+//  barij awsan value-uudiig object bolgono
+//  tom array-ruu object-iig push hiine
+//  popup window disappear
+//  render fn duudaj ajiluulna
+
+//render fn:
+//  tom array-g filterdeed 4 jijig array bolgono
+//  todo bolgoniig div uusgeed
+//  dotor ni content-uudaa hiij ogood
+//  cards dotor card bolhoj hiine
+
+//  board name
+//  pop up screen title
+//  p that counts card
+//  css border + center + padding
+//  edit, delete button name
+//  drop down
+//  sort by priority
+//edit fn
+//  button-giin unique id-g barij awna.
+//  array deeguur map erguuleed id ni taarch baiwal value-uudiig ni barij awna
+//  pop-up-iin input-nuudiig barij awsan value-uudaar solino
+//  pop-up-iig gargaj irne
+//  garch irsen medeelliig garaar oorchilno
+//  add task towch darhad huuuchin id-nii object-iig ni ustgana
+//  add task fn (pop-up) ajiluulna
+//delete fn
+//  button-giin unique id-g barij awna.
+//  array deeguur filter erguuleed id-tai taarahgui id-uudiig ni awaad tom array-uudruugaa assign hiine
+//  render fn ajiluulna.
+
+//  Remaining----------------------------------------------------------------
+
+//done when checked
+//  button-giin unique id-g barij awna.
+//  array deeguur map erguuleed status-iig ni done bolgono
+//  render fn ajiluulna.
+
+//  drag and drop
+//  if add task properties == "" { not able to press the button, and make input red}
+//  modal
+
+// function helper(element, className, idName, appendParent) {
+//   if (element != "" && appendParent != "") {
+//     let temp = document.createElement(`${element}`);
+//     temp.setAttribute("class", `${className}`);
+//     temp.setAttribute("id", `${idName}`);
+//     k.appendChild(temp);
+//   }
+//   return temp;
+// }
+// let testingElement = helper("p", "", "testClass", root);
+
+// Hig
+// Low
+// Mid
--- script.js
+++ script.js
@@ -1,58 +1,22 @@
-// function helper(element, divName, calssName, idName, append) {
-//   let element = document.createElement(divName);
-//   element.setAttribute("class", calssName);
-//   element.setAttribute("class", idName);
-//   append.appendChild(element);
-// }
-// let taskArr = [
-//   {
-//     title: "Task1",
-//     description: "Task1",
-//     status: "To Do",
-//     priority: "Low",
-//     id: 1,
-//     button: "Add Task",
-//   },
-//   {
-//     title: "Task2",
-//     description: "Task2",
-//     status: "In-progress",
-//     priority: "Low",
-//     id: 2,
-//     button: "Add Task",
-//   },
-//   {
-//     title: "Task3",
-//     description: "Task3",
-//     status: "Stuck",
-//     priority: "Low",
-//     id: 3,
-//     button: "Add Task",
-//   },
-//   {
-//     title: "Task4",
-//     description: "Task4",
-//     status: "Done",
-//     priority: "Low",
-//     id: 4,
-//     button: "Add Task",
-//   },
-// ];\
-debugger;
+// asuultiin nohtsoloos hamaarch ali function we gedgiig shiidne. (edit hiih uyd add task darhad)
+
 // root ee duudan
-const root = document.getElementById("root");
-boardArr = ["To-Do", "In-progress", "Stuck", "Done"];
+let root = document.getElementById("root");
+let boardsArr = ["To-Do", "In-progress", "Stuck", "Done"];
+taskArr = [];
+let idNum = 1;
 
 // boards hiij bn
 let boards = document.createElement("div");
 boards.setAttribute("class", "boards");
 root.appendChild(boards);
 
-boardArr.map((event) => {
+boardsArr.map((event) => {
   // board iig hiij bn
   let board = document.createElement("div");
   board.setAttribute("class", "board");
-  board.setAttribute("id", `${event}`);
+  // -------------------------------------------------
+  board.setAttribute("id", event);
   boards.appendChild(board);
 
   // board header
@@ -62,57 +26,300 @@ boardArr.map((event) => {
   header.innerText = `${event}`;
   board.appendChild(header);
 
+  // count number
+  let countNum = document.createElement("p");
+  countNum.setAttribute("id", `count${event}`);
+  board.appendChild(countNum);
+  // -------------------------------------
+  boards.appendChild(board);
+
   // board card
-  let card = document.createElement("div");
-  card.setAttribute("class", "card");
-  card.setAttribute("id", `${event}`);
-  board.appendChild(card);
+  let cards = document.createElement("div");
+  cards.setAttribute("class", "cards");
+  cards.setAttribute("id", `cards${event}`);
+  board.appendChild(cards);
 
   // board button
   let button1 = document.createElement("button");
   button1.setAttribute("class", "button");
   button1.innerText = "+ add card";
   board.appendChild(button1);
+  button1.addEventListener("click", showPopup);
 });
 
-// add task screen
-let taskTitle = ["Title", "Description", "Status", "Priority"];
-
+// add task screen hiij bgaa heseg
+let taskProperties = ["Title", "Description", "Status", "Priority"];
 let popUp = document.createElement("div");
 popUp.setAttribute("class", "popUp");
 root.appendChild(popUp);
 
-const popUpHeader = document.createElement("h1");
-popUpHeader.setAttribute("class", "popUpHeader");
-popUpHeader.innerText = "Add Task";
-popUp.appendChild(popUpHeader);
+const popupHeader = document.createElement("h1");
+popupHeader.setAttribute("class", "popUpHeader");
+popupHeader.setAttribute("id", "popupHeader");
+popupHeader.innerText = "Add Task";
+popUp.appendChild(popupHeader);
 
 // popUp -iin div -iig hiij bn
-taskTitle.map((event) => {
-  // div -iin hiij bn
-  let property = document.createElement("div");
-  property.setAttribute("class", "property");
-  property.setAttribute("id", `${event}`);
-  popUp.appendChild(property);
+taskProperties.map((event) => {
+  if (event == "Title" || event == "Description") {
+    // div -iin hiij bn
+    let property = document.createElement("div");
+    property.setAttribute("class", "property");
+    property.setAttribute("id", `${event}`);
+    popUp.appendChild(property);
 
-  // label
-  let label = document.createElement("label");
-  label.setAttribute("class", "label");
-  label.setAttribute("id", `${event}`);
-  label.innerText = `${event}`;
-  property.appendChild(label);
+    // label-property dororh contentuudiig hiij bn
+    let label = document.createElement("label");
+    label.setAttribute("class", "label");
+    label.innerText = `${event}`;
+    property.appendChild(label);
 
-  // input
-  let input = document.createElement("input");
-  input.setAttribute("class", "input");
-  property.appendChild(input);
+    // input-property dororh contentuudiig hiij bn
+    let input = document.createElement("input");
+    input.setAttribute("class", "input");
+    input.setAttribute("id", `input${event}`);
+    property.appendChild(input);
+  } else if (event == "Status") {
+    //div-uudiig hiilee
+    let property = document.createElement("div");
+    property.setAttribute("class", "property");
+    property.setAttribute("id", `${event}`);
+    popUp.appendChild(property);
+
+    //property dotorhi content-uudiig hiij baigaa
+    let label = document.createElement("label");
+    label.setAttribute("class", "label");
+    label.innerText = `${event}`;
+    property.appendChild(label);
+
+    //property dotorhi content-uudiig hiij baigaa
+    let input = document.createElement("select");
+    input.setAttribute("class", "input");
+    input.setAttribute("id", `input${event}`);
+
+    // option1
+    let option1 = document.createElement("option");
+    option1.value = `To do`;
+    option1.innerText = `To do`;
+    input.appendChild(option1);
+
+    // option2
+    let option2 = document.createElement("option");
+    option2.value = `In progress`;
+    option2.innerText = `In progress`;
+    input.appendChild(option2);
+
+    // option3
+    let option3 = document.createElement("option");
+    option3.value = `Stuck`;
+    option3.innerText = `Stuck`;
+    input.appendChild(option3);
+    // option4
+    let option4 = document.createElement("option");
+    option4.value = `Done`;
+    option4.innerText = `Done`;
+    input.appendChild(option4);
+    //
+    property.appendChild(input);
+  } else if (event == "Priority") {
+    //div-uudiig hiilee
+    let property = document.createElement("div");
+    property.setAttribute("class", "property");
+    property.setAttribute("id", `${event}`);
+    popUp.appendChild(property);
+
+    //property dotorhi content-uudiig hiij baigaa
+    let label = document.createElement("label");
+    label.setAttribute("class", "label");
+    label.innerText = `${event}`;
+    property.appendChild(label);
+
+    let input = document.createElement("select");
+    input.setAttribute("class", "input");
+    input.setAttribute("id", `input${event}`);
+
+    // op1
+    let option1 = document.createElement("option");
+    option1.value = "High";
+    option1.innerText = `High`;
+    input.appendChild(option1);
+
+    // op2
+    let option2 = document.createElement("option");
+    option2.value = "Medium";
+    option2.innerText = `Medium`;
+    input.appendChild(option2);
+
+    // op3
+    let option3 = document.createElement("option");
+    option3.value = "Low";
+    option3.innerText = `Low`;
+    input.appendChild(option3);
+    property.appendChild(input);
+  }
 });
 
 // poUp button
-const popUpBtn = document.createElement("button");
-popUpBtn.setAttribute("class", "popUpBtn");
-popUpBtn.innerText = "Add Task";
-popUp.appendChild(popUpBtn);
+const popupButton = document.createElement("button");
+popupButton.setAttribute("class", "popUpBtn");
+popupButton.setAttribute("id", "popupButton");
+popupButton.innerText = "Add Task";
 
-//
-debugger;
+// popupButton.setAttribute("click", addTask);
+popupButton.addEventListener("click", addTaskButtF);
+popUp.appendChild(popupButton);
+// alga bolgoh
+popUp.style.display = "none";
+
+//add card darhad popUp garchirh
+function showPopup() {
+  popUp.style.display = "block";
+}
+
+function addTaskButtF() {
+  let tempObj = {
+    id: idNum,
+    Title: document.getElementsByClassName("input")[0].value,
+    Description: document.getElementsByClassName("input")[1].value,
+    Status: document.getElementsByClassName("input")[2].value,
+    Priority: document.getElementsByClassName("input")[3].value,
+  };
+
+  if (document.getElementsByClassName("input")[3].value == "High") {
+    tempObj.PriorNum = 3;
+  } else if (document.getElementsByClassName("input")[3].value == "Medium") {
+    tempObj.PriorNum = 2;
+  } else if (document.getElementsByClassName("input")[3].value == "Low") {
+    tempObj.PriorNum = 1;
+  }
+
+  // let tempPri = document.getElementsByClassName("input")[3].value;
+  taskArr.push(tempObj);
+  console.log(taskArr);
+
+  //task-iin input-iig hoosloj baigaaa
+  document.getElementsByClassName("input")[0].value = "";
+  document.getElementsByClassName("input")[1].value = "";
+  document.getElementsByClassName("input")[2].value = "";
+  document.getElementsByClassName("input")[3].value = "";
+
+  popUp.style.display = "none";
+  idNum++;
+  arrangeAndRender;
+}
+//arrange and render fn:
+//  cards dotor card bolhoj hiine
+function arrangeAndRender() {
+  //shine card nemhiin omno huuchin card-uudiig ustgaj baigaa
+  for (let i = 0; i < 4; i++) {
+    document.getElementById(`cards${boardsArr[i]}`).innerHTML = "";
+  }
+  //  tom array-g filterdeed 4 jijig array bolgono
+  let todoArr = [];
+  for (let i = 0; i < 4; i++) {
+    todoArr[i] = taskArr.filter((event) => {
+      return event.Status == boardsArr[i];
+    });
+
+    todoArr[i].sort((a, b) => {
+      return b.PriorNum - a.PriorNum;
+    });
+  }
+
+  // count number ajiluulj hiij baigaa heseg
+  todoArr.map((event, index) => {
+    let countNum = document.getElementById(`count${boardsArr[index]}`);
+    if (event.length == 0) {
+      countNum.innerText = `0`;
+    } else {
+      countNum.innerText = `${event.length}`;
+    }
+  });
+
+  //  card-iin div uusgeed
+  //  dotor ni content-uudaa hiij ogj baigaa heseg
+  //  cards-ruu hiij baigaa
+  for (let i = 0; i < 4; i++) {
+    todoArr[i].map((event) => {
+      let card = document.createElement("div");
+      card.setAttribute("class", "card");
+
+      let tick = document.createElement("button");
+      tick.innerText = `Done`;
+      card.appendChild(tick);
+
+      let head = document.createElement("h3");
+      head.innerText = `${event.Title}`;
+      card.appendChild(head);
+
+      let desc = document.createElement("p");
+      desc.innerText = `${event.Description}`;
+      card.appendChild(desc);
+
+      let prio = document.createElement("button");
+      prio.innerText = `${event.Priority}`;
+      card.appendChild(prio);
+
+      // edit
+      let edit = document.createElement("button");
+      edit.setAttribute("id", `${event.id}`);
+      edit.addEventListener("click", (event) => {
+        let id = event.target.id;
+        editF(id);
+      });
+      edit.innerText = `Edit`;
+      card.appendChild(edit);
+
+      // delete
+      let dele = document.createElement("button");
+      dele.innerText = `Delete`;
+      dele.setAttribute("id", `${event.id}`);
+      dele.addEventListener("click", (event) => {
+        let id = event.target.id;
+        deleteF(id);
+      });
+      card.appendChild(dele);
+
+      //zow cards-ruu zow array-iig ni oruulj ogj baigaa heseg
+      document.getElementById(`cards${boardsArr[i]}`).appendChild(card);
+    });
+  }
+  document.getElementById("popupButton").innerText = "Add task";
+  document.getElementsByClassName("popupHeader").innerText = "Add task";
+}
+function editF(id) {
+  let tempObj = taskArr.filter((event) => {
+    return event.id == id;
+  });
+
+  for (let i = 0; i < 4; i++) {
+    if (i == 0) {
+      document.getElementById(`input${taskProperties[i]}`).value =
+        tempObj[0].Title;
+    } else if (i == 1) {
+      document.getElementById(`input${taskProperties[i]}`).value =
+        tempObj[0].Description;
+    } else if (i == 2) {
+      document.getElementById(`input${taskProperties[i]}`).value =
+        tempObj[0].Status;
+    } else if (i == 3) {
+      document.getElementById(`input${taskProperties[i]}`).value =
+        tempObj[0].Priority;
+    }
+  }
+
+  taskArr = taskArr.filter((event) => {
+    return event.id != id;
+  });
+  document.getElementById("popupButton").innerText = "Confirm Edit";
+  document.getElementById("popupHeader").innerText = "Edit task";
+
+  showPopup();
+}
+
+function deleteF(id) {
+  taskArr = taskArr.filter((event) => {
+    return event.id != id;
+  });
+  arrangeAndRender();
+}
